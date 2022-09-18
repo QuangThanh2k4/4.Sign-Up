ee925840926c87ed7c79cb61d431af2877f3b2eb
QuangThanh2k4 <nguyenquang101104@gmailcom>
2022-09-18 20:32:51 +0700

    gửi bài tạp lên cho anh trung chữa

--- 4.Sign Up/ViewController.swift
+++ 4.Sign Up/ViewController.swift
@@ -8,12 +8,71 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+  
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var phoneField: UITextField!
+    @IBOutlet weak var dateofbirthField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var button: UIButton!
+    
+    
+    
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+
     }
+   
+        @IBAction func buttonTapped() {
+        }
+    
+    
 
 
+}
+
+extension UIView {
+    
+
+  @IBInspectable var cornerRadius: CGFloat {
+    get {
+      return layer.cornerRadius
+    }
+    set {
+      layer.cornerRadius = newValue
+    }
+  }
+}
+
+extension UITextField {
+  
+  @IBInspectable var paddingLeftCustom: CGFloat {
+    get {
+      return leftView!.frame.size.width
+    }
+    set {
+      let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: newValue, height: frame.size.height))
+      leftView = paddingView
+      leftViewMode = .always
+    }
+  }
+  
+  @IBInspectable var paddingRightCustom: CGFloat {
+    get {
+      return rightView!.frame.size.width
+    }
+    set {
+      let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: newValue, height: frame.size.height))
+      rightView = paddingView
+      rightViewMode = .always
+    }
+  }
 }
 
